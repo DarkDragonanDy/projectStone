d5e172e952044999f2104a072bb357d497b41540
Wimage03 <nuru@usf.edu>
2022-10-16 20:57:11 -0400

    more hello worlds

--- Assets/Scripts/MONO.cs
+++ Assets/Scripts/MONO.cs
@@ -20,6 +20,10 @@ public class MONO : MonoBehaviour
         Debug.Log("Hello world");
         Debug.Log("Hello world");
         Debug.Log("Hello world");
+        Debug.Log("Hello world");
+        Debug.Log("Hello world");
+        Debug.Log("Hello world");
+
         
         
         
